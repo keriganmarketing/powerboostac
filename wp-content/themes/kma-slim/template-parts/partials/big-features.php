36a1e5031d291dd1a3a802588fde45fcbb2af66f
programmer-man <bryan@kerigan.com>
2018-02-26 15:54:11 -0500

    line breaks

--- wp-content/themes/kma-slim/template-parts/partials/big-features.php
+++ wp-content/themes/kma-slim/template-parts/partials/big-features.php
@@ -5,21 +5,21 @@
                 <img src="<?php echo get_template_directory_uri() . '/img/cooling@2x.png'; ?>">
             </div>
             <h2 class="title is-3">Better A/C Cooling</h2>
-            <p class="subtitle is-6">Lowers temperatures with  same energy output.</p>
+            <p class="subtitle is-6">Lowers temperatures<br>with  same energy output.</p>
         </div>
         <div class="column is-4 energy">
             <div class="image">
                 <img src="<?php echo get_template_directory_uri() . '/img/energy@2x.png'; ?>">
             </div>
             <h2 class="title is-3">Less Energy Usage</h2>
-            <p class="subtitle is-6">Lowers electricity and maintenance costs.</p>
+            <p class="subtitle is-6">Lowers electricity and<br>maintenance costs.</p>
         </div>
         <div class="column is-4 diy">
             <div class="image">
                 <img src="<?php echo get_template_directory_uri() . '/img/DIY@2x.png'; ?>">
             </div>
             <h2 class="title is-3">Do-it-yourself application</h2>
-            <p class="subtitle is-6">One simple set up provides lifetime system performance.</p>
+            <p class="subtitle is-6">One simple set up provides <br>lifetime system performance.</p>
         </div>
     </div>
 </div>
