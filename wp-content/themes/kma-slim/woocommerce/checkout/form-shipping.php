4596f9f6eef6790f52ce348f5ad94595e719d6f9
programmer-man <bryan@kerigan.com>
2018-03-22 18:17:40 -0400

    checkout form tweaks

--- wp-content/themes/kma-slim/woocommerce/checkout/form-shipping.php
+++ wp-content/themes/kma-slim/woocommerce/checkout/form-shipping.php
@@ -24,16 +24,15 @@ if ( ! defined('ABSPATH')) {
 <div class="woocommerce-shipping-fields">
     <?php if (true === WC()->cart->needs_shipping_address()) : ?>
 
-        <h3 id="ship-to-different-address">
-            <label class="woocommerce-form__label woocommerce-form__label-for-checkbox checkbox">
-                <input id="ship-to-different-address-checkbox"
-                       class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox" <?php checked(apply_filters('woocommerce_ship_to_different_address_checked',
-                    'shipping' === get_option('woocommerce_ship_to_destination') ? 1 : 0), 1); ?> type="checkbox"
-                       name="ship_to_different_address" value="1"/> <span class="title is-3 is-secondary"><?php _e('Ship to a different address?',
-                        'woocommerce'); ?></span>
-            </label>
-        </h3>
+        <h3 id="ship-to-different-address" class="title is-3 is-secondary"><?php _e('Shipping',
+                'woocommerce'); ?></h3>
 
+        <p id="ship-to-different-address">
+            <label class="woocommerce-form__label woocommerce-form__label-for-checkbox checkbox">
+                <input id="ship-to-different-address-checkbox" class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox" <?php checked( apply_filters( 'woocommerce_ship_to_different_address_checked', 'shipping' === get_option( 'woocommerce_ship_to_destination' ) ? 1 : 0 ), 1 ); ?> type="checkbox" name="ship_to_different_address" value="1" /> <span><?php _e( 'Ship to a different address?', 'woocommerce' ); ?></span>
+            </label>
+        </p>
+<p></p>
         <div class="shipping_address">
 
             <?php do_action('woocommerce_before_checkout_shipping_form', $checkout); ?>
@@ -57,6 +56,7 @@ if ( ! defined('ABSPATH')) {
 
     <?php endif; ?>
 </div>
+<p></p>
 <div class="woocommerce-additional-fields">
     <?php do_action('woocommerce_before_order_notes', $checkout); ?>
 
