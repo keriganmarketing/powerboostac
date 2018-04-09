49aead45d10c6ef3ced728e031f99f23f36df63e
Dylan <johnston94@gmail.com>
2018-04-09 16:44:40 -0500

    Update header and 3 col on homepage

--- wp-content/themes/kma-slim/template-parts/sections/home-header.php
+++ wp-content/themes/kma-slim/template-parts/sections/home-header.php
@@ -3,13 +3,13 @@
         <div class="columns hero-content is-mobile">
             <div class="column hero-item is-5-tablet is-4-desktop">
                 <h1 class="title is-1">Stay Cool & Save Money</h1>
-                <p class="subtitle">PowerBoost AC is a remarkably simple, proven way to keep your home cooler using less energy and saving you money.</p>
+                <p class="subtitle">PowerBoost AC is a remarkably simple, proven way to keep your home cooler with less energy and lower electric bills.</p>
                 <div class="columns is-aligned is-mobile">
                     <div class="column is-narrow">
-                        <strong>Only $295</strong>
+                        <a href="/product/power-boost-kit/" class="button is-primary" >Buy Now</a>
                     </div>
                     <div class="column">
-                        <a href="/product/power-boost-kit/" class="button is-primary" >Buy Now</a>
+                        <a href="#" class="button is-secondary" >Watch Video</a>
                     </div>
                 </div>
             </div>
