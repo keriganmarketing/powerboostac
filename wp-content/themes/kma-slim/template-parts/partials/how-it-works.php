824742546b82a3e15b2d9b2f58e3879c438cc7c3
programmer-man <bryan@kerigan.com>
2018-04-25 10:52:26 -0400

    added video

--- wp-content/themes/kma-slim/template-parts/partials/how-it-works.php
+++ wp-content/themes/kma-slim/template-parts/partials/how-it-works.php
@@ -1,11 +1,15 @@
 <a name="video"></a>
-<div class="container"> 
-    <h2 class="title is-1"><strong>The science is simple.</strong></h2>
+<div class="container">
     <div class="columns is-multiline is-aligned">
         <div class="column is-6 is-second-desktop">
-            <img class="logo" src="<?php echo get_template_directory_uri() . '/img/desktop/video@2x.png'; ?>">
+            <figure class="video is-16by9 has-border has-shadow">
+                <div class="embed-wrapper">
+                <iframe src="https://www.youtube-nocookie.com/embed/tUgGxobJMBs?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                </div>
+            </figure>
         </div>
         <div class="column is-6 is-first-desktop">
+            <h2 class="title is-1"><strong>The science is simple.</strong></h2>
             <p>Every AC has lubricating oil running through the unit — but when oil film and deposits cling to the inside of your copper tubing, you lose performance. PowerBoost AC is scientifically engineered to dislodge oil fouling and prevent lubricating oil from ever reattaching to the metal surfaces again. Learn more or watch the video.</p>
             <p class="section-action"><a class="button is-info" href="/faqs/">FAQs</a></p>
         </div>
