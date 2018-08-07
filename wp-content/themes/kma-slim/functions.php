bd99cd63dd497c207af58b8f4ab8c8eeaee862bf
programmer-man <bryan@kerigan.com>
2018-08-07 18:03:05 -0400

    sale text

--- wp-content/themes/kma-slim/functions.php
+++ wp-content/themes/kma-slim/functions.php
@@ -106,3 +106,9 @@ add_filter('woocommerce_checkout_fields', function ($fields) {
 
     return $fields;
 });
+
+add_filter('woocommerce_sale_flash', 'woocommerce_custom_sale_text', 10, 3);
+function woocommerce_custom_sale_text($text, $post, $_product)
+{
+    return '<span class="onsale">Summer Special</span>';
+}
