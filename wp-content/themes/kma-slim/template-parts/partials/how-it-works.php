ce90a03cef9ec642c6cd4f24ae0840b8a8564c64
Robert Crites III <robertcritesiii@Roberts-MacBook-Pro.local>
2018-12-28 01:27:41 -0600

    Content updates

--- wp-content/themes/kma-slim/template-parts/partials/how-it-works.php
+++ wp-content/themes/kma-slim/template-parts/partials/how-it-works.php
@@ -1,17 +1,36 @@
-<div class="container">
-    <a name="video" id="video" class="pad-anchor">&nbsp;</a>
-    <div class="columns is-multiline is-aligned">
-        <div class="column is-6 is-second-desktop">
-            <figure class="video is-16by9 has-border has-shadow">
-                <div class="embed-wrapper">
-                <iframe src="https://www.youtube-nocookie.com/embed/tUgGxobJMBs?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                </div>
-            </figure>
+<div class="why-koolmaxx">
+    <div class="container">
+        <div class="why-message">
+            <h1>Why Use KoolMaxx?</h1>
+            <p>Why Use KoolMaxx? KoolMaxx is an additive that removes oil fouling from your air conditioning system. Oil fouling will cause a loss of about 7% efficiency in the first year of an air conditioner’s life, up to a loss of 30% to 40% efficiency over time. By removing oil fouling, the refrigerant in your air conditioner flows throughout the system without being restricted – this leads to a more efficient system that provides cooler air while extending compressor life.</p>
         </div>
-        <div class="column is-6 is-first-desktop">
-            <h2 class="title is-1"><strong>The science is simple.</strong></h2>
-            <p>Every AC has lubricating oil running through the unit — but when oil film and deposits cling to the inside of your copper tubing, you lose performance. PowerBoost AC is scientifically engineered to dislodge oil fouling and prevent lubricating oil from ever reattaching to the metal surfaces again. We’re so confident that PowerBoost AC will pay for itself, we <a href="https://powerboostac.com/wp-content/uploads/2018/06/PowerBoost-AC-warranty.pdf" target="_blank">Guarantee It</a>!</p>
-            <p class="section-action"><a class="button is-info" href="/faqs/">FAQs</a></p>
+        <div class="columns hero-content is-mobile">
+            <div class="column is-4">
+                <div class="card">
+                    <h2>LESS WEAR AND TEAR</h2>
+                    <p>KoolMaxx helps your A/C operate with less wear  and tear, which extends compressor life and saves on repair costs.</p>
+                </div>
+                <div class="card">
+                    <h2>HOME AND BUSINESS</h2>
+                    <p>KoolMaxx is used to improve the efficiency in your home and business A/C system, and can be used right after installation.</p>
+                </div>
+                <div class="card">
+                    <h2>LESS FRICTION</h2>
+                    <p>KoolMaxx works by improving the lubricity in your air conditioning system, creating less friction in the compressor.</p>
+                </div>
+                <div class="card">
+                    <h2>REDUCE LOAD</h2>
+                    <p>KoolMaxx is an additive that removes oil fouling, which allows for better heat transfer – reducing the load on the compressor.</p>
+                </div>
+                <div class="card">
+                    <h2>COOLER AIR</h2>
+                    <p>KoolMaxx will decrease your home and business air temperature by 3 to 9 degrees.  Stay cool during hot months while improving efficiency!</p>
+                </div>
+                <div class="card">
+                    <h2>IMPROVED PERFORMANCE</h2>
+                    <p>With KoolMaxx, your air conditioning system will have improved performance while providing cooler air.  It’s a no lose investment in your comfort!</p>
+                </div>
+            </div>
         </div>
     </div>
 </div>
