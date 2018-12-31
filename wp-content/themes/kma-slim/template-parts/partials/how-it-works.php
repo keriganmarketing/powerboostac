af81d1398c8f7f1edd48017d164ba9fea509d1de
Bryan Baird <bryan@kerigan.com>
2018-12-31 11:55:36 -0500

    name change

--- wp-content/themes/kma-slim/template-parts/partials/how-it-works.php
+++ wp-content/themes/kma-slim/template-parts/partials/how-it-works.php
@@ -10,7 +10,7 @@
         </div>
         <div class="column is-6 is-first-desktop">
             <h2 class="title is-2">The science is simple.</h2>
-            <p>Every AC has lubricating oil running through the unit — but when oil film and deposits cling to the inside of your copper tubing, you lose performance. PowerBoost AC is scientifically engineered to dislodge oil fouling and prevent lubricating oil from ever reattaching to the metal surfaces again. We’re so confident that PowerBoost AC will pay for itself, we <a href="https://powerboostac.com/wp-content/uploads/2018/06/PowerBoost-AC-warranty.pdf" target="_blank">Guarantee It</a>!</p>
+            <p>Every AC has lubricating oil running through the unit — but when oil film and deposits cling to the inside of your copper tubing, you lose performance. KoolMaxx AC is scientifically engineered to dislodge oil fouling and prevent lubricating oil from ever reattaching to the metal surfaces again. We’re so confident that KoolMaxx AC will pay for itself, we <a href="https://powerboostac.com/wp-content/uploads/2018/06/PowerBoost-AC-warranty.pdf" target="_blank">Guarantee It</a>!</p>
             <p class="section-action"><a class="button is-info" href="/faqs/">FAQs</a> <a class="button is-primary" href="/videos" >More Videos</a></p>
         </div>
     </div>
