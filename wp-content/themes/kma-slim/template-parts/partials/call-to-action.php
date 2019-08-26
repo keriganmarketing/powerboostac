2bbd344faf2ba5c5c6bf03be689dac9fb30c42ce
Bryan Baird <bryan@kerigan.com>
2019-08-26 12:50:19 -0400

    change contact info

--- wp-content/themes/kma-slim/template-parts/partials/call-to-action.php
+++ wp-content/themes/kma-slim/template-parts/partials/call-to-action.php
@@ -2,9 +2,8 @@
 <a name="form" id="form" class="pad-anchor"></a>
     <div class="has-text-centered has-text-white ">
         <h2 class="title is-1 has-text-white">Contact Us</h2>
-        <p class="has-text-white has-text-weight-bold">Mike Perry</p>
-        <p class="has-text-white has-text-weight-bold">Vice President</p>
-        <p class="has-text-white"><a class="has-text-white has-text-weight-bold" href="tel:2516560507">(251) 656-0507</a></p>
+        <p class="has-text-white has-text-weight-bold">Dennis Foreman</p>
+        <p class="has-text-white"><a class="has-text-white has-text-weight-bold" href="tel:2514554224">(251) 455-4224 </a></p>
         &nbsp;
     </div>
     <?php echo do_shortcode('[contact_form]'); ?>
--- wp-content/themes/kma-slim/template-parts/partials/how-it-works.php
+++ wp-content/themes/kma-slim/template-parts/partials/how-it-works.php
@@ -13,17 +13,19 @@
     <div class="container">
     <a name="video" id="video" class="pad-anchor">&nbsp;</a>
     <div class="columns is-multiline is-aligned">
-        <div class="column is-6 is-second-desktop">
+        <!-- <div class="column is-6 is-second-desktop">
             <figure class="video is-16by9 has-border has-shadow">
                 <div class="embed-wrapper">
                 <iframe src="https://www.youtube-nocookie.com/embed/<?php echo $videoId; ?>?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                 </div>
             </figure>
-        </div>
-        <div class="column is-6 is-first-desktop">
+        </div> -->
+        <div class="column is-8">
             <h2 class="title is-2">The science is simple.</h2>
             <p>Every AC has lubricating oil running through the unit — but when oil film and deposits cling to the inside of your copper tubing, you lose performance. KoolMaxx AC is scientifically engineered to dislodge oil fouling and prevent lubricating oil from ever reattaching to the metal surfaces again. </p>
-            <p class="section-action"><a class="button is-info" href="/faqs/">FAQs</a> <a class="button is-primary" href="/videos" >More Videos</a></p>
+        </div>
+        <div class="column is-4 has-text-centered">
+        <p class="section-action"><a class="button is-info is-large" href="/faqs/">FAQs</a></p>
         </div>
     </div>
 </div>
